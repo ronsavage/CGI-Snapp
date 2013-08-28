ea74a1a2f783b1284a8eb905290b60135d8c0034
ronsavage <ron@savage.net.au>
2013-08-28 16:27:39 +1000

    Re-work code in params.pl

--- t/params.pl
+++ t/params.pl
@@ -31,12 +31,16 @@ my($app)   = CGI::Snapp -> new(logger => $logger, send_output => 0);
 
 # Get the default params, of which there are none.
 
+=pod
+
 diag 'Start test 1';
 diag '-' x 20;
 diag Dumper([$app -> param]);
 diag '-' x 20;
 diag Dumper([]);
 diag '-' x 20;
+
+=cut
 
 cmp_deeply([$app -> param], [], 'No params are set by default'); $count++;
 
@@ -48,6 +52,8 @@ $app -> param(%old_params);
 
 my(%new_params) = map{($_ => $app -> param($_) )} $app -> param;
 
+=pod
+
 diag 'Start test 2';
 diag '-' x 20;
 diag Dumper(\%old_params);
@@ -55,9 +61,13 @@ diag '-' x 20;
 diag Dumper(\%new_params);
 diag '-' x 20;
 
+=cut
+
 cmp_deeply([sort %old_params], [sort %new_params], 'Can set and get a hash of params'); $count++;
 
 $app -> delete($_) for keys %old_params;
+
+=pod
 
 diag 'Start test 3';
 diag '-' x 20;
@@ -65,6 +75,8 @@ diag Dumper([$app -> param]);
 diag '-' x 20;
 diag Dumper([]);
 diag '-' x 20;
+
+=cut
 
 cmp_deeply([$app -> param], [], 'No params are set after mass delete'); $count++;
 
@@ -76,6 +88,8 @@ $app -> param($old_params);
 
 %new_params = map{($_ => $app -> param($_) ) } $app -> param;
 
+=pod
+
 diag 'Start test 4';
 diag '-' x 20;
 diag Dumper($old_params);
@@ -83,9 +97,13 @@ diag '-' x 20;
 diag Dumper(\%new_params);
 diag '-' x 20;
 
+=cut
+
 cmp_deeply([sort %$old_params], [sort %new_params], 'Can set and get a hash of params'); $count++;
 
 $app -> delete($_) for keys %$old_params;
+
+=pod
 
 diag 'Start test 5';
 diag '-' x 20;
@@ -93,6 +111,8 @@ diag Dumper([$app -> param]);
 diag '-' x 20;
 diag Dumper([]);
 diag '-' x 20;
+
+=cut
 
 cmp_deeply([$app -> param], [], 'No params are set after mass delete'); $count++;
 
@@ -110,6 +130,8 @@ $app -> param(%old_params);
 
 my($value) = $app -> param(%old_params);
 
+=pod
+
 diag 'Start test 6';
 diag '-' x 20;
 diag Dumper($value);
@@ -117,7 +139,11 @@ diag '-' x 20;
 diag Dumper(5);
 diag '-' x 20;
 
+=cut
+
 ok($value == 5, 'param($key => $value) returns that value');                $count++;
+
+=pod
 
 diag 'Start test 7';
 diag '-' x 20;
@@ -126,11 +152,15 @@ diag '-' x 20;
 diag Dumper($app -> param('five') );
 diag '-' x 20;
 
+=cut
+
 ok($value == $app -> param('five'), 'param($key) also returns that value'); $count++;
 
 %old_params = (one => 1, two => 2, three => 3, four => 4, %old_params);
 
 %new_params = map{($_ => $app -> param($_) )} $app -> param;
+
+=pod
 
 diag 'Start test 8';
 diag '-' x 20;
@@ -139,12 +169,16 @@ diag '-' x 20;
 diag Dumper(\%new_params);
 diag '-' x 20;
 
+=cut
+
 cmp_deeply([sort %old_params], [sort %new_params], 'Params match after being added in stages'); $count++;
 
 # What is returned after delete()?
 
 my($key) = 'five';
 $value   = $app -> delete($key);
+
+=pod
 
 diag 'Start test 9';
 diag '-' x 20;
@@ -153,7 +187,11 @@ diag '-' x 20;
 diag Dumper(5);
 diag '-' x 20;
 
+=cut
+
 ok($value == 5, "delete($key) returns the corresponding value: $value");            $count++;
+
+=pod
 
 diag 'Start test 10';
 diag '-' x 20;
@@ -162,9 +200,13 @@ diag '-' x 20;
 diag '$VAR1 = undef;';
 diag '-' x 20;
 
+=cut
+
 ok(! defined $app -> param($key), "After delete($key), param($key) returns undef"); $count++;
 
 $value = $app -> delete($key);
+
+=pod
 
 diag 'Start test 11';
 diag '-' x 20;
@@ -173,17 +215,21 @@ diag '-' x 20;
 diag '$VAR1 = undef;';
 diag '-' x 20;
 
+=cut
+
 ok(! defined $value, "After delete($key) twice, delete() returns undef"); $count++;
 
 # Clean up before testing an arrayref.
 
 $app -> delete($_) for keys %old_params;
 
-$old_params = [qw/six 6 seven 7/];
+$old_params = ['six' => 6, 'seven' => 7];
 
 $app -> param($old_params);
 
 %new_params = map{($_ => $app -> param($_) )} $app -> param;
+
+=pod
 
 diag 'Start test 12';
 diag '-' x 20;
@@ -191,6 +237,8 @@ diag Dumper($old_params);
 diag '-' x 20;
 diag Dumper([%new_params]);
 diag '-' x 20;
+
+=cut
 
 cmp_deeply([sort @$old_params], [sort %new_params], 'Params match after being added via an arrayref'); $count++;
 
